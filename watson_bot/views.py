5062bd91dab7d23619a7ea4ddc98db792f600f0b
Niko <nick.oshinov@gmail.com>
2019-07-11 16:38:41 -0400

    Changed the way fb payload is formatted

--- watson_bot/views.py
+++ watson_bot/views.py
@@ -30,11 +30,12 @@ SESSION_TIMEOUT = 5*60 - 10 # Session timeout after this long
 def send_message(recipient_id, message):
     endpoint = f"{FACEBOOK_ENDPOINT}?access_token={FB_PAGE_ACCESS_TOKEN}"
     payload = json.dumps(
-        {"recipient":
-            { 
-                "messaging_type": "RESPONSE",
-                "id": recipient_id}, 
-                "message": {"text": message} }
+        {
+            "messaging_type": "RESPONSE",
+            "recipient": { "id": recipient_id },
+            "message": { "text": message } 
+            }
+
         )
 
     status = requests.post(
