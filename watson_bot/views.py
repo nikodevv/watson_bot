1a8dad1df1533c3642b396d798213a25b29552b1
Niko <nick.oshinov@gmail.com>
2019-07-11 22:00:23 -0400

    removed debug statements

--- watson_bot/views.py
+++ watson_bot/views.py
@@ -70,12 +70,10 @@ class FacebookWebhookView(View):
         min_timestamp_before_timeout = time.time() - SESSION_TIMEOUT
         sender_id = self.get_sender_id(facebook_entry)
 
-        print("2222222")
         recent_msgs = Message.objects.filter(
             timestamp__gt=min_timestamp_before_timeout,
             sender_id__exact=sender_id)
 
-        print("3333333")
         if (len(recent_msgs) == 0):
             session_id = watson.create_session()["session_id"]
             session = self.save_session(session_id)
@@ -86,12 +84,10 @@ class FacebookWebhookView(View):
             session = self.save_session(session_id)
         
         else: 
-            print("CONDITION 3")
             session = recent_msgs[0].session
             self.renew_session(session.session_id)
 
 
-        print("4444444")
         message = self.save_message(facebook_entry, session)
         watson_response = watson.send_message(message.text, session.session_id)
         self.save_watson_response(watson_response, sender_id)
@@ -127,8 +123,6 @@ class FacebookWebhookView(View):
         message.text = text
         message.id = id
         message.session = session
-        print("555555555")
-        print(timestamp)
         message.save()
 
         return message
@@ -146,7 +140,6 @@ class FacebookWebhookView(View):
         return HttpResponse("EVENT_RECIEVED")
 
     def get(self, request, *args, **kwargs):
-        print("WHY IS THIS CAlled")
         return HttpResponse(request.GET.get('hub.challenge'))
 
 class DjangoRunsView(View):
